1eed58b96beb9719e7e4ffd12378cd28ace5e7c2
carmar04 <2dam.csoro@institutoserpis.org>
2018-10-15 13:52:51 +0200

    Añadido lista con objetos creada a partir de clases a parte

---
+++ SpinnerClases/app/src/main/java/com/example/carmar04/spinnerclases/Titular.java
@@ -0,0 +1,33 @@
+package com.example.carmar04.spinnerclases;
+
+public class Titular {
+    private String titulo, subtitulo;
+    private int drawable;
+
+    public Titular(){
+    }
+    public Titular(String titulo, String subtitulo, int drawable){
+        this.titulo = titulo;
+        this.subtitulo = subtitulo;
+        this.drawable = drawable;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public void setSubtitutlo(String subtitulo){
+        this.subtitulo = subtitulo;
+    }
+    public String getSubtitulo(){
+        return this.subtitulo;
+    }
+    public void setDrawable(int drawable){
+        this.drawable = drawable;
+    }
+    public int getDrawable(){
+        return this.drawable;
+    }
+
+}
